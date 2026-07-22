f623d407bf3a8721cc0d2368f6c41ed93d650fee
Slava Gorbunov <slava@fizlesh.org.ru>
2026-07-22 18:03:43 +0300

    Multiple kiosks

--- modules/kiosk-flake.nix
+++ modules/kiosk-flake.nix
@@ -1,135 +1,147 @@
 { config, lib, inputs, uinstall, disko, ... }:
 
 let
-  cfg = config.kiosk;
-  effectiveCommand = if lib.isDerivation cfg.command
-    then "${lib.getExe cfg.command}"
-    else cfg.command;
+  mkKiosk = name: kcfg:
+    let
+      effectiveCommand = if lib.isDerivation kcfg.command
+        then "${lib.getExe kcfg.command}"
+        else kcfg.command;
+    in
+    {
+      packages = { pkgs, ... }:
+      let
+        builtinQtileConfig = ./qtileconf.py;
+        effectiveQtileConfig = if kcfg.qtileConfig == null
+          then builtinQtileConfig
+          else kcfg.qtileConfig;
+
+        scripts = import ./scripts.nix {
+          inherit pkgs lib;
+          command = effectiveCommand;
+          qtileConfigPath = effectiveQtileConfig;
+          rotate = kcfg.rotate;
+        };
+      in
+      {
+        "kiosk-${name}" = scripts.launcher;
+      };
+
+      nixosConfig = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules =
+          lib.optionals kcfg.includeInstaller [
+            disko.nixosModules.default
+            uinstall.nixosModules.simpleDisko
+          ]
+          ++ [
+            ./kiosk.nix
+            ({ pkgs, ... }: {
+              kiosk = {
+                enable = true;
+                command = effectiveCommand;
+                user = kcfg.user;
+                qtileConfig = kcfg.qtileConfig;
+                rotate = kcfg.rotate;
+              };
+
+              services.openssh.settings.permitRootLogin = false;
+              users.users.root.password = kcfg.rootPassword;
+
+              environment.systemPackages = with pkgs; [
+                iw wirelesstools psmisc
+              ];
+
+              networking.networkmanager = lib.mkIf (kcfg.wifiNetworks != {}) {
+                enable = true;
+                ensureProfiles.profiles = lib.mapAttrs (ssid: psk: {
+                  connection = {
+                    id = ssid;
+                    type = "wifi";
+                  };
+                  ipv4.method = "auto";
+                  ipv6 = {
+                    addr-gen-mode = "stable-privacy";
+                    method = "auto";
+                  };
+                  wifi = {
+                    mode = "infrastructure";
+                    ssid = ssid;
+                  };
+                  wifi-security = {
+                    auth-alg = "open";
+                    key-mgmt = "wpa-psk";
+                    psk = psk;
+                  };
+                }) kcfg.wifiNetworks;
+              };
+            })
+          ];
+      };
+    };
 in
 {
   imports = [ uinstall.flakeModules.default ];
 
-  options.kiosk = {
-    enable = lib.mkEnableOption "kiosk mode with greetd + qtile + bwrap sandbox";
+  options.kiosk = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.submodule ({ name, ... }: {
+      options = {
+        command = lib.mkOption {
+          type = lib.types.either lib.types.package lib.types.str;
+          description = "Application to run inside qtile (package or shell command string)";
+        };
 
-    command = lib.mkOption {
-      type = lib.types.either lib.types.package lib.types.str;
-      description = "Application to run inside qtile (package or shell command string)";
-    };
+        user = lib.mkOption {
+          type = lib.types.str;
+          default = "kiosk";
+          description = "Auto-login user";
+        };
 
-    hostName = lib.mkOption {
-      type = lib.types.str;
-      default = "kiosk";
-      description = "NixOS hostname";
-    };
+        rootPassword = lib.mkOption {
+          type = lib.types.str;
+          default = "kiosk";
+          description = "Root password for the NixOS VM";
+        };
 
-    user = lib.mkOption {
-      type = lib.types.str;
-      default = "kiosk";
-      description = "Auto-login user";
-    };
+        wifiNetworks = lib.mkOption {
+          type = lib.types.attrsOf lib.types.str;
+          default = {};
+          description = "WiFi networks: SSID -> PSK";
+        };
 
-    rootPassword = lib.mkOption {
-      type = lib.types.str;
-      default = "kiosk";
-      description = "Root password for the NixOS VM";
-    };
+        qtileConfig = lib.mkOption {
+          type = lib.types.nullOr lib.types.path;
+          default = null;
+          description = "Custom qtile config; uses builtin if null";
+        };
 
-    wifiNetworks = lib.mkOption {
-      type = lib.types.attrsOf lib.types.str;
-      default = {};
-      description = "WiFi networks: SSID -> PSK";
-    };
+        includeInstaller = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Include disko + uinstall modules for unattended NixOS installation";
+        };
 
-    qtileConfig = lib.mkOption {
-      type = lib.types.nullOr lib.types.path;
-      default = null;
-      description = "Custom qtile config; uses builtin if null";
-    };
-
-    includeInstaller = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "Include disko + uinstall modules for unattended NixOS installation";
-    };
-
-    rotate = lib.mkOption {
-      type = lib.types.enum [ 0 90 180 270 ];
-      default = 0;
-      description = "Screen rotation in degrees";
-    };
+        rotate = lib.mkOption {
+          type = lib.types.enum [ 0 90 180 270 ];
+          default = 0;
+          description = "Screen rotation in degrees";
+        };
+      };
+    }));
+    default = {};
   };
 
-  config = lib.mkIf cfg.enable {
-
+  config = {
     perSystem = { pkgs, ... }:
     let
-      builtinQtileConfig = ./qtileconf.py;
-      effectiveQtileConfig = if cfg.qtileConfig == null
-        then builtinQtileConfig
-        else cfg.qtileConfig;
-
-      scripts = import ./scripts.nix {
-        inherit pkgs lib;
-        command = effectiveCommand;
-        qtileConfigPath = effectiveQtileConfig;
-        rotate = cfg.rotate;
-      };
+      all = lib.mapAttrs (name: kcfg: mkKiosk name kcfg) config.kiosk;
+      packages = lib.mapAttrs' (name: k: lib.nameValuePair "kiosk-${name}" (k.packages { inherit pkgs; })) all;
+      # Flatten nested attrset: {"scroller": {"kiosk-scroller": <drv>}} -> {"kiosk-scroller": <drv>}
     in
     {
-      packages.default = scripts.launcher;
+      packages = lib.foldl' (a: b: a // b) {} (lib.attrValues packages);
     };
 
-    flake.nixosConfigurations."${cfg.hostName}" = inputs.nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules =
-        lib.optionals cfg.includeInstaller [
-          disko.nixosModules.default
-          uinstall.nixosModules.simpleDisko
-        ]
-        ++ [
-          ./kiosk.nix
-          ({ pkgs, ... }: {
-            kiosk = {
-              enable = true;
-              command = effectiveCommand;
-              user = cfg.user;
-              qtileConfig = cfg.qtileConfig;
-              rotate = cfg.rotate;
-            };
-
-            services.openssh.settings.permitRootLogin = false;
-            users.users.root.password = cfg.rootPassword;
-
-            environment.systemPackages = with pkgs; [
-              iw wirelesstools psmisc
-            ];
-
-            networking.networkmanager = lib.mkIf (cfg.wifiNetworks != {}) {
-              enable = true;
-              ensureProfiles.profiles = lib.mapAttrs (ssid: psk: {
-                connection = {
-                  id = ssid;
-                  type = "wifi";
-                };
-                ipv4.method = "auto";
-                ipv6 = {
-                  addr-gen-mode = "stable-privacy";
-                  method = "auto";
-                };
-                wifi = {
-                  mode = "infrastructure";
-                  ssid = ssid;
-                };
-                wifi-security = {
-                  auth-alg = "open";
-                  key-mgmt = "wpa-psk";
-                  psk = psk;
-                };
-              }) cfg.wifiNetworks;
-            };
-          })
-        ];
-    };
+    flake.nixosConfigurations =
+      lib.mapAttrs (name: kcfg: (mkKiosk name kcfg).nixosConfig) config.kiosk;
   };
 }
